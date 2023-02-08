beb482205f6df143889142cb7de9fc0bd1338d1b
Hasibul294 <hasibulhasan294@gmail.com>
2023-02-08 14:19:39 +0600

    add counter slice

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,28 @@
+// Slice - In redux slice is collection of logic. any kind of features
+// logic will reside in this slice file
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByValue: (state, action) => {
+      state.count = state.count + action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, reset, incrementByValue } =
+  counterSlice.actions;
+
+export default counterSlice.reducer;
